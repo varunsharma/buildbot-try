9ac2511944a325ae6ef1fefaec9cb2ad0b997341
Varun Sharma <varun@sharmalabs.org>
2015-07-05 15:52:28 +0530

    Initialize the buildot-try, the try client for Buildbot

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'buildbot-try',
+  packages = ['buildbot-try'],
+  version = '0.1',
+  description = 'Buildbot Try Client',
+  author = 'Varun Sharma',
+  author_email = 'varun@sharmalabs.org',
+  url = 'https://github.com/varunsharma/buildbot-try',
+#  download_url = 'https://github.com/varunsharma/buildbot-try/tarball/0.1',
+  keywords = ['buildbot', 'try client', 'continous inegration'],
+  classifiers = [],
+)
